42b53f9826ed78cfb71f0076c930d6523857abe6
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2022-01-06 06:17:29 -0300

    styling the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
+import styles from './styles.module.scss';
+
 export function Header(){
   return (
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="Ig.news"  layout="fill" />
         <nav>
           <a>Home</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import Head from 'next/head'
-import { Header } from '../components'
 export default function Home() {
   return (
-    <>
       <Head>
           <title>Início | ig.news</title>
       </Head>
-      <Header />
-    </>
   )
 }

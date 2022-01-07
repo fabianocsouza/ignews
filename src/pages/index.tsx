b4f37d528d2fe360b07298e1ed33080d8b5a5287
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2022-01-07 06:10:36 -0300

    Styling the sign in Github button and creating home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
+import Image from 'next/image'
 export default function Home() {
   return (
+    <>
       <Head>
-          <title>Início | ig.news</title>
+          <title>Home | ig.news</title>
       </Head>
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+        </section>
+        <Image src="/images/avatar.svg" alt="Girl coding" width="334" height="520" />
+      </main>
+    </>
   )
 }

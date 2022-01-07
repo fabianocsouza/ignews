12dbfbcd666f7da2070428eb409244c783da1bec
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2022-01-06 22:16:57 -0300

    styling the header and button sign in github

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 
+import { SignInButton } from "../SignInButton";
+
 import styles from './styles.module.scss';
 
 export function Header(){
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="Ig.news"  layout="fill" />
+        <Image src="/images/logo.svg" alt="Ig.news" width="108.45" height="30.27"/>
         <nav>
-          <a>Home</a>
+          <a className={styles.active} >Home</a>
           <a>Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   )

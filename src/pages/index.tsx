307f90f651cd0f876c7946250979d20f2fa8ed72
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2022-01-11 06:37:20 -0300

    service creation with stripe, subscribe button and home page with styling.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+
+import { SubscribeButton } from '../components/SubscribeButton';
+
 import styles from './home.module.scss';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <Head>
@@ -17,9 +21,18 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <Image src="/images/avatar.svg" alt="Girl coding" width="334" height="520" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return { 
+    props: {
+      name: 'Fabiano Souza'
+    }
+  }
 }
